eb95803a4ff71ce0c4e26e7f4cc5d9cdb97d8870
Kyle Stigler <kstigle1@binghamton.edu>
2020-03-16 20:12:44 -0400

    part one all but last print line

--- program1.cpp
+++ program1.cpp
@@ -21,56 +21,88 @@ int main(int argc, char *argv[])
 	fileX >> X;
 	fileY >> Y;
 	
-	box table[X.size()][Y.size()];
-	box setX, setY;
-	setX.len = 0;
-	setX.dir = "NA";
-	setY.len = 0;
-	setY.dir = "NA";
+	int p = X.size();
+	int q = Y.size();
 
-	for (int i=0; i<X.size(); i++)
+	box table[p+1][q+1];
+	box set0;
+	set0.len = 0;
+	set0.dir = "@";
+
+	for (int i=0; i<p+1; i++)
 	{
-		table[i][0] = setX;
+		table[i][0] = set0;
 	}
-	for (int j=1; j<Y.size(); j++)
+	for (int j=1; j<q+1; j++)
 	{
-		table[0][j] = setY;
+		table[0][j] = set0;
 	}
 	
-	for (int r=1; r<X.size(); r++)
+	for (int r=1; r<p+1; r++)
 	{
-		for (int c=1; c<Y.size(); c++)
+		for (int c=1; c<q+1; c++)
 		{
 			box added;
 			if (X.at(r-1) == Y.at(c-1))
 			{
-				added.len = table[r-1][c-1] + 1;
+				added.len = table[r-1][c-1].len + 1;
 				added.dir = "D";
 				table[r][c] = added;
 			}
 			else
 			{
-				if (table[r-1][c] > table[r][c-1])
+				if (table[r-1][c].len > table[r][c-1].len)
 				{
-					added.len = table[r-1][c];
+					added.len = table[r-1][c].len;
 					added.dir = "U";
 					table[r][c] = added;
 				}
 				else
 				{
-					added.len = table[r][c-1];
+					added.len = table[r][c-1].len;
 					added.dir = "L";
 					table[r][c] = added;
 				}
 			}
 		}
 	}
-	//backtrack from here
+	
 
 	ofstream outFile;
 	outFile.open(argv[3]);
 
-	outFile << X << ", " << Y << endl;
+	if (p <= 10 || q <= 10)
+	{
+		for (int a=0; a<p+1; a++)
+		{
+			for (int b=0; b<q+1; b++)
+			{	
+				outFile << table[a][b].len << ", " << table[a][b].dir << "  ";
+			}
+			outFile << endl;
+		}
+		string answer;
+		while (p != 0 && q != 0)
+		{
+			if (table[p][q].dir == "D")
+			{
+				string convert;
+				convert += X.at(p-1);
+				answer.insert(0, convert);
+				p--;
+				q--;
+			}
+			else if (table[p][q].dir == "U")
+			{
+				p--;
+			}
+			else
+			{
+				q--;
+			}
+		}
+		outFile << answer << endl;
+	}
 	
 	return 0;
 }
